e3ecfbcd77c3f6e2ed503e641ae23fd0b0856c96
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-15 10:06:30 +0800

    Create class02.java

---
+++ class02.java
@@ -0,0 +1,13 @@
+public class class02{
+    public static void main(String[] args) {
+        int num = 12;
+        int den = 0;
+        if (den == 0) {
+            System.out.println("den is 0");
+        }
+        else {
+            int ans = num / den;
+            System.out.println("Result: " + ans);
+        }
+    }    
+}
